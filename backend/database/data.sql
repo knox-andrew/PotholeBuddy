3cbd673ff52f01056bfb24d16d5f9b7a06d446da
Andrew Knox <knoxar@gmail.com>
2019-08-07 10:58:47 -0400

    Adding different api

--- backend/database/data.sql
+++ backend/database/data.sql
@@ -1,6 +1,19 @@
 -- Password for this user is 'greatwall'
-INSERT INTO users ("username", "password", "salt", "role") VALUES
-('user',
+INSERT INTO users ("id", "username", "password", "salt", "role") VALUES
+(0,
+'user',
 'FjZDm+sndmsdEDwNtfr6NA==',
 'kidcasB0te7i0jK0fmRIGHSm0mYhdLTaiGkEAiEvLp7dAEHWnuT8n/5bd2V/mqjstQ198iImm1xCmEFu+BHyOz1Mf7vm4LILcrr17y7Ws40Xyx4FOCt8jD03G+jEafpuVJnPiDmaZQXJEpEfekGOvhKGOCtBnT5uatjKEuVWuDA=',
 'user');
+
+INSERT INTO markers ("user_id", "latitude", "longitude", "rating", "comments") 
+VALUES (0, '39.193684289509065', '-84.32208179208072', 'moderate', 
+        'Not too bad. Located on the left side of the road.');
+
+INSERT INTO markers ("user_id", "latitude", "longitude", "rating", "comments") 
+VALUES (0, '39.22214908061195', '-84.36568378182682', 'severe', 
+        'Fairly deep and in the middle of the road. Definitely avoid.');
+
+INSERT INTO markers ("user_id", "latitude", "longitude", "rating", "comments") 
+VALUES (0, '39.22214908061195', '-84.36568378182682', 'critical', 
+        'Very deep and covers almost half the road. Cracked 2 of my rims');
--- backend/database/schema.sql
+++ backend/database/schema.sql
@@ -1,6 +1,7 @@
 BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS markers;
 
 CREATE TABLE users (
   id serial PRIMARY KEY,
@@ -8,6 +9,15 @@ CREATE TABLE users (
   password varchar(32) NOT NULL,      -- Password
   salt varchar(256) NOT NULL,          -- Password Salt
   role varchar(255) NOT NULL default('user')
+);
+
+CREATE TABLE markers (
+    id serial PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    latitude varchar(64) NOT NULL,
+    longitude varchar(64) NOT NULL,
+    rating varchar(16) NOT NULL,
+    comments varchar(256) DEFAULT ''
 );
 
 COMMIT TRANSACTION;
