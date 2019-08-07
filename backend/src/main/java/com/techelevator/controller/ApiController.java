59cdab794d75ebabcad27c5b5c5b70d3ae1a2e3b
Andrew Knox <knoxar@gmail.com>
2019-08-07 10:11:20 -0400

    backend

---
+++ backend/src/main/java/com/techelevator/controller/ApiController.java
@@ -0,0 +1,24 @@
+package com.techelevator.controller;
+
+import com.techelevator.authentication.AuthProvider;
+import com.techelevator.authentication.UnauthorizedException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * ApiController
+ */
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+    @Autowired
+    private AuthProvider authProvider;
+
+    @RequestMapping(path = "/", method = RequestMethod.GET)
+    public String authorizedOnly() throws UnauthorizedException {
+        return "Success";
+    }
+}
